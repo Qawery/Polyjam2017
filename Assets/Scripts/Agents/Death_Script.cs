4ff5120cef253381d941ef5b2569ecbd76b640dd
Qawery <wooki1@op.pl>
2017-01-22 12:20:05 +0100

    Wybuch czolgu.

--- Assets/Scripts/Agents/Death_Script.cs
+++ Assets/Scripts/Agents/Death_Script.cs
@@ -1,22 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Death_Script : MonoBehaviour {
-
+public class Death_Script : MonoBehaviour
+{
     private float timer;
 
-	// Use this for initialization
-	void Start () {
+	public void Start ()
+    {
         timer = 0f;
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
-        timer += Time.deltaTime;
-       
-		
+    public void Update ()
+    {
+        timer += Time.deltaTime;      	
         if(timer > 3f)
         {
             Destroy(gameObject);
