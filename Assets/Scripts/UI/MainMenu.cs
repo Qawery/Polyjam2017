f1e7da985c008d5cae05380c93e99095dc215ebc
Qawery <wooki1@op.pl>
2017-01-22 07:47:45 +0100

    Main menu done.

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public Canvas mainMenu;
+    public Canvas howToPlay;
+
+    public void Start()
+    {
+        mainMenu.enabled = true;
+        howToPlay.enabled = false;
+    }
+
+    public void StartButton()
+    {
+        Application.LoadLevel("level_01");
+    }
+
+    public void howToPlayButton()
+    {
+        mainMenu.enabled = false;
+        howToPlay.enabled = true;
+    }
+
+    public void backButton()
+    {
+        mainMenu.enabled = true;
+        howToPlay.enabled = false;
+    }
+
+    public void exitButton()
+    {
+        Application.Quit();
+    }
+}
