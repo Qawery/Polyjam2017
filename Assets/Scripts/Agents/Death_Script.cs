d786959f5e41169e415df6e28d8e587ecf6ff9a6
Mandemik <harewind@gmail.com>
2017-01-22 11:40:15 +0100

    Pomalowana mapa, dodane dzwieki

---
+++ Assets/Scripts/Agents/Death_Script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death_Script : MonoBehaviour {
+
+    private float timer;
+
+	// Use this for initialization
+	void Start () {
+        timer = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timer += Time.deltaTime;
+       
+		
+        if(timer > 3f)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
